cad78eeae7a0973376bf3b5a8dea6ab021d82983
SavageDeveloper247 <69088477+SavageDeveloper247@users.noreply.github.com>
2022-08-29 14:54:41 -0300

    Add files via upload

---
+++ steam1.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+window.location = "https://steamcommunity.com/sharedfiles/filedetails/?id=2855907280";
+setTimeout(function(){
+        SubscribeItem();
+    }, 1000);
+})();
